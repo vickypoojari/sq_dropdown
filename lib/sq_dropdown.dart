e9e0e1ffb7f8d95f7e607e7f4cb2508fdcb56045
Vicky P <vicky.p@thesmartq.com>
2023-09-01 15:32:38 +0530

    Made some changes

--- lib/sq_dropdown.dart
+++ lib/sq_dropdown.dart
@@ -1,8 +1,28 @@
 library sq_dropdown;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+import 'package:flutter/material.dart';
 
+/// A Calculator.
+class SqDropDown extends StatelessWidget {
+  const SqDropDown({super.key, required this.onPressed, required this.child});
+
+  final VoidCallback onPressed;
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(16.0),
+          primary: Colors.white,
+          backgroundColor: Colors.blue,
+          elevation: 9.0,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        child: child);
+  }
+}
